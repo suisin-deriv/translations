37f875dbf855f895ee92337639c8a56f886bb7c7
amir ali <amir.ali@regentmarkets.com>
2024-05-23 11:47:29 +0500

    chore: update deprecation message for localize function

--- src/utils/localize.ts
+++ src/utils/localize.ts
@@ -2,6 +2,8 @@ import { str as crc32 } from "crc-32";
 import i18next from "i18next";
 
 /**
+ * @deprecated use the `localize` function from the `useTranslations` hook, this function will not get the update from i18n instance once there is any changes like resource loaded or language changed
+ *
  * @param {string} string
  * @param {Record<string, unknown>} values
  * @returns {string}
